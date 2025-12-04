d7afecb49df383b69d0b638759d849c984dcb677
Shikhil Rane <shikhilrane04@gmail.com>
2025-12-04 17:44:56 +0530

    updated code for LL and its notes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-
+        LinkedList<Integer> ll = new LinkedList<>(Arrays.asList(42, 7));
+        System.out.println(ll);
     }
 }
