40c5ba8679565f599deb834320ebc7fc1a0ea795
Shikhil Rane <shikhilrane04@gmail.com>
2025-12-04 15:55:17 +0530

    Added more information in collection framework

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,5 @@
-import java.util.Enumeration;
-import java.util.Vector;
-
 public class Main {
     public static void main(String[] args) {
-        Vector v = new Vector();
-        v.add(10);
-        v.add("SKR");
-        v.add("15.2");
 
-        Enumeration e = v.elements();
-        while (e.hasMoreElements()){
-            System.out.println(e.nextElement());
-        }
     }
 }
