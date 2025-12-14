e35ef007d3f743012238606b0e7c87a4f55f1fc5
Shikhil Rane <shikhilrane04@gmail.com>
2025-12-14 14:12:11 +0530

    Understanding of CopyOnWriteArrayList

--- src/_23_Collection_Framework/_01_Collection_Interface/_01_List_Interface/_05_CopyOnWriteArrayList/UnderstandingCOWAL/Cowal.java
+++ src/_23_Collection_Framework/_01_Collection_Interface/_01_List_Interface/_05_CopyOnWriteArrayList/UnderstandingCOWAL/Cowal.java
@@ -1,9 +1,25 @@
 package _23_Collection_Framework._01_Collection_Interface._01_List_Interface._05_CopyOnWriteArrayList.UnderstandingCOWAL;
 
+import java.util.ArrayList;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Cowal {
     public static void main(String[] args) {
+        // Modifications while reading ArrayList
+        ArrayList<String> arraylist = new ArrayList<>();
+        arraylist.add("Milk");
+        arraylist.add("Egg");
+        arraylist.add("Bread");
+
+        for (String item : arraylist){
+            System.out.println(item);
+//            if (item.equals("Egg")){                              // It will give exception because it doesn't allow to write anything while reading
+//                arraylist.add("Butter");
+//                System.out.println("Added Butter while reading");
+//            }
+        }
+
+        // Modifications while reading CopyOnWriteArrayList
         CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
 
         // Adding elements
@@ -16,7 +32,7 @@ public class Cowal {
             System.out.println(item);
         }
 
-        // Safe iteration while modifying
+        // Modifying list while Reading elements
         for (String item : list) {
             if (item.equals("B")) {
                 list.remove("B");  // No ConcurrentModificationException
@@ -26,3 +42,12 @@ public class Cowal {
         System.out.println("After removal: " + list);
     }
 }
+
+// 'Copy On Write' means that whenever a write operation like adding or removing an element
+// Instead of directly modifying the existing list, a new copy of list is created and modification is applied to that copy
+// This ensures that other thread reading the list while its being modified are unaffected.
+
+// Read Operations : Fast and direct, since they have happened on stable list without interference from modifications
+// Write Operations : A new copy of list is created for every modification, The reference to the list is then updated so that subsequent reads this new list
+
+// CopyOnWriteArrayList is used when write operations are less and read operations are more, because it consumes more memory as it create ArrayList for every modification
