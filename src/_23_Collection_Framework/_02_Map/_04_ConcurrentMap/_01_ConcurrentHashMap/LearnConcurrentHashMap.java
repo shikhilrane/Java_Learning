12c7d2574dd8edc63bc9b2b9048174663821dfeb
Shikhil Rane <shikhilrane04@gmail.com>
2025-12-19 21:43:31 +0530

    added ConcurrentMap

---
+++ src/_23_Collection_Framework/_02_Map/_04_ConcurrentMap/_01_ConcurrentHashMap/LearnConcurrentHashMap.java
@@ -0,0 +1,10 @@
+package _23_Collection_Framework._02_Map._04_ConcurrentMap._01_ConcurrentHashMap;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class LearnConcurrentHashMap {
+    public static void main(String[] args) {
+        ConcurrentHashMap<String, Integer> concurrentHashMap = new ConcurrentHashMap<>();
+
+    }
+}
---
+++ src/_23_Collection_Framework/_02_Map/_04_ConcurrentMap/_02_ConcurrentSkipListMap/LearnConcurrentSkipListMap.java
@@ -0,0 +1,4 @@
+package _23_Collection_Framework._02_Map._04_ConcurrentMap._02_ConcurrentSkipListMap;
+
+public class LearnConcurrentSkipListMap {
+}
